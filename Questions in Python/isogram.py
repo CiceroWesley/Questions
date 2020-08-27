314b096bdeaa68b5d231fce11d5ff4946c6b7c2c
Cicero Wesley <wesleycariutaba@gmail.com>
2020-08-27 13:45:26 -0300

    pyhon questions

---
+++ Questions in Python/isogram.py
@@ -0,0 +1,17 @@
+def is_isogram(string):
+  stringls = string.lower().strip()
+<<<<<<< HEAD
+  retorno=1
+  for i in range(0,len(stringls)):
+    if stringls[i] != " " and stringls[i]!= "-":
+      if stringls.count(stringls[i]) > 1:
+        return False
+      else:
+        retorno = 1
+  if retorno == 1:
+    return True        
+
+
+
+print(is_isogram(""))
+
